52d09e53a4b4537fb7d02ce14fe2d5879c2ac0d2
Zarnab Raza <141522632+ZarnabRaza@users.noreply.github.com>
2024-08-15 18:12:51 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version 
+
+alert (2+2); // we are using node Js not browser
+
+console.log(3 + 3); console.log("Hitesh"); // code readibility should be high
+
+// Data Types in Javascript
+let name = "Zarnab";
+let age = 18;  //(type number , range 2 power of 53)
+let isLoggedIn = false;
+
+
+
+// Prmitive Data types
+// String
+// Number
+// boolean
+// Bigint (bohat baray number ko bigInt khetay hain)
+// boolean true/false
+// null // null is a stand alone value // undefined nahi hai khali hai// null reprenstation hai empty ka , undefined nahi hai khali hai
+// undefined // abhi value assign nahi hoi hai
+// symbol => unique
+
+
+
+// Object
+// null ka typeof object hai 
+// undefined ka output  typeof undefined hai 
+
+
+
+
+
+
+
+
